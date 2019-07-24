4598304dd2dd2fd546b8c8a7bd54fc6defd915b0
Chris Hansen <cmhhansen@outlook.com>
2019-07-24 23:08:33 +1200

    updates

--- bigdata/data/nycflights.sql
+++ bigdata/data/nycflights.sql
@@ -3,36 +3,21 @@ create database nycflights;
 use nycflights;
 
 CREATE TABLE IF NOT EXISTS airlines (carrier STRING, name STRING) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( "quoteChar" = "\"", "separatorChar" = ",") 
-STORED AS TEXTFILE;
-
-LOAD DATA LOCAL INPATH '/data/csv/airlines.csv' INTO TABLE airlines;
+STORED AS ORC;
+LOAD DATA LOCAL INPATH '/data/orc/nycflights/airlines/' INTO TABLE airlines;
 
 CREATE TABLE IF NOT EXISTS airports (faa STRING, name STRING, lat FLOAT, lon FLOAT, alt INT, tz INT, dst STRING, tzone STRING) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( "quoteChar" = "\"", "separatorChar" = ",") 
-STORED AS TEXTFILE;
-
-LOAD DATA LOCAL INPATH '/data/csv/airports.csv' INTO TABLE airports;
+STORED AS ORC;
+LOAD DATA LOCAL INPATH '/data/orc/nycflights/airports/' INTO TABLE airports;
 
 CREATE TABLE IF NOT EXISTS flights (year INT, month INT, day INT, dep_time INT, sched_dep_time INT, dep_delay INT, arr_time INT, sched_arr_time INT, arr_delay INT, carrier STRING, flight INT, tailnum STRING, origin STRING, dest STRING, air_time INT, distance INT, hour INT, minute INT, time_hour TIMESTAMP) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( "quoteChar" = "\"", "separatorChar" = ",") 
-STORED AS TEXTFILE;
+STORED AS ORC;
+LOAD DATA LOCAL INPATH '/data/orc/nycflights/flights/' INTO TABLE flights;
 
-LOAD DATA LOCAL INPATH '/data/csv/flights.csv' INTO TABLE flights;
-
-CREATE TABLE IF NOT EXISTS planes (tailnum STRING, year INT, type INT, manufacturer STRING, model STRING, engines INT, seats INT, speed INT, engine STRING) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( "quoteChar" = "\"", "separatorChar" = ",") 
-STORED AS TEXTFILE;
-
-LOAD DATA LOCAL INPATH '/data/csv/planes.csv' INTO TABLE planes;
+CREATE TABLE IF NOT EXISTS planes (tailnum STRING, year INT, type STRING, manufacturer STRING, model STRING, engines INT, seats INT, speed INT, engine STRING) 
+STORED AS ORC;
+LOAD DATA LOCAL INPATH '/data/orc/nycflights/planes/' INTO TABLE planes;
 
 CREATE TABLE IF NOT EXISTS weather (origin STRING, year INT, month INT, day INT, hour INT, temp FLOAT, dewp FLOAT, humid FLOAT, wind_dir INT, wind_speed FLOAT, wind_gust FLOAT, precip FLOAT, pressure FLOAT, visib FLOAT, time_hour TIMESTAMP) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( "quoteChar" = "\"", "separatorChar" = ",") 
-STORED AS TEXTFILE;
-
-LOAD DATA LOCAL INPATH '/data/csv/weather.csv' INTO TABLE weather;
+STORED AS ORC;
+LOAD DATA LOCAL INPATH '/data/orc/nycflights/weather/' INTO TABLE weather;
--- bigdata/files/init.sh
+++ bigdata/files/init.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+service ssh start
+service postgresql start
+
+hdfs namenode -format
+/opt/hadoop/sbin/start-all.sh
+/opt/spark/sbin/start-master.sh
+/opt/spark/sbin/start-slave.sh localhost:7077
+
 if [ ! -d "/data/hive/metastore_db" ]; then
   echo "Setting up metastore"
   cd /data/hive
@@ -11,6 +19,7 @@ $SPARK_HOME/sbin/start-thriftserver.sh --master "local[4]" --conf "spark.sql.shu
 $HIVE_HOME/hcatalog/sbin/hcat_server.sh start 
 
 hive -f /data/nycflights.sql
+source /data/nycflights.sh
 
 mkdir -p ${HOME}/.jupyter
 touch ${HOME}/.jupyter/jupyter_notebook_config.py 
